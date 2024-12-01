e1b9f902a5d6937a9a292559f06b37fe4ce4ca54
HarshRai8800 <harshrai8800>
2024-12-01 22:55:08 +0530

    third commit

--- frontend/src/page/Signup.tsx
+++ frontend/src/page/Signup.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+
 import Input from '../components/Input'
 import { useState } from 'react'
-import { data, useNavigate } from 'react-router-dom'
+import {  useNavigate } from 'react-router-dom'
 import axios from 'axios'
 function Signup() {
   const navigate = useNavigate()
